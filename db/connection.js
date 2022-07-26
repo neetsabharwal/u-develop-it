4a231cd31749d9310bf6666ccfb19fd1b84ce9b7
Gurneet <gsingh2@atb.com>
2022-07-26 14:21:40 -0400

    modularized

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "root",
+    // Your MySQL password
+    password: "password",
+    database: "election",
+  },
+);
+
+module.exports = db;
